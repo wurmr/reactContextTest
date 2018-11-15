9cdef51ca78dbe93c8d4bac0915eaf496d1103f8
Jim Karg <jkarg@intertech.com>
2018-11-15 15:52:15 -0600

    Context demo

--- src/App.js
+++ src/App.js
@@ -1,27 +1,55 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { PureComponent, Component } from 'react';
 import './App.css';
 
+const Context = React.createContext('default')
+
 class App extends Component {
+  state = {
+    value: 'original'
+  }
+
+  componentDidMount() {
+    setTimeout(() => this.setState({value: 'updated' }), 1500)
+  }
+
   render() {
+    console.log('rendering app')
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <div className="App">
+          <Context.Provider value={this.state.value}>
+            <FirstMiddleMan />
+          </Context.Provider>
+        </div>
+
     );
+  }
+}
+
+const FirstMiddleMan = React.memo(() => { 
+  console.log('rendering 1st middle man')
+  return <div><SecondMiddleMan /></div>
+})
+
+
+
+class SecondMiddleMan extends Component {
+  render() { 
+    console.log('rendering 2nd middle man')
+    return <div><ThirdMiddleMan /></div>
+  }
+}
+
+const ThirdMiddleMan = () => { 
+  console.log('rendering 3rd middle man')
+  return <div><Consumer /></div>
+}
+
+class Consumer extends Component {
+  static contextType = Context
+
+  render() {
+    console.log('rendering consumer')
+    return <div>{this.context}</div>
   }
 }
 
